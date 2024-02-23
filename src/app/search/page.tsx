aebfddc0972afc9c6754c2291dcb1d8b0ee7b328
Michał Pielak <toja@example.com>
2024-02-24 00:26:17 +0100

    Poprawna implementacja strony search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,41 +1,23 @@
-'use client'
- 
-import { useEffect, useState } from 'react';
 import { ProductList } from '../../app/ui/organism/ProductList';
-import type { ProductOnListItemType } from '../../app/ui/types';
 import { getProductsBySearch } from '@/api/products';
 
-export default function SearchProductsPage() {
-    const [search, setSearch] = useState<string | null>(null);
-    const [products, setProducts] = useState<ProductOnListItemType[]>([]); 
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                if (search) {
-                    const data = await getProductsBySearch(search);
-                    setProducts(data);
-                }
-            } catch (error) {
-                console.error('Error fetching products:', error);
-            }
-        };
+interface SearchParams {
+    query: string;
+}
 
-        if (search) {
-            fetchData().catch(error => {
-                console.error('Error fetching products:', error);
-            });
-        }
-    }, [search]);
 
-    useEffect(() => {
-        const searchParams = new URLSearchParams(window.location.search);
-        setSearch(searchParams.get('query'));
-    }, []);
+interface Props {
+    searchParams: SearchParams;
+}
 
+
+export default async function SearchProductsPage({ searchParams }: Props) {
+    const { query } = searchParams;    
+    const products = await getProductsBySearch(query);
     return (
         <>
-            <p>Search: {search}</p>
+            <p>Search: {query}</p>
             <ProductList products={products} />
         </>
     );
