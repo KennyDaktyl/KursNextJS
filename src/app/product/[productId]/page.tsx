1c8311c803e7926b79bbdc985bbea69a35acf3bb
mpielak <michal.pielak81@gmail.com>
2024-02-29 18:43:14 +0100

    Refactor zadania z wyszukiwarką

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
-import { ProductDetails } from "@/app/ui/molecules/ProductDetails";
+import { CheckIcon } from "lucide-react";
+
 import { getProductById, getProductIdForStaticPage } from "@/api/products";
 import type { ProductIdForStaticPageType, ProductOnListItemType } from "@/app/ui/types";
 import { getCollectionProductsBySlug } from "@/api/collections";
 import { ProductList } from "@/app/ui/organism/ProductList";
+import { ProductImage } from "@/app/ui/atoms/ProductImage";
+import { formatMoney } from "@/utils";
 
 
 export const generateStaticParams = async () => {
@@ -63,15 +66,54 @@ export default async function ProductDetailsPage({
     );
     const containerName = "related-products";
 
+    async function AddToCartAction(_formData: FormData) {
+        "use server";
+    
+        console.log(params.productId)
+    }
+
     return (
-        <>
-            <ProductDetails product={ product }/>
+        <article className="mx-auto grid max-w-7xl py8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-2">
+                <div>
+                    <ProductImage 
+                        src={ product.images.url }
+                        alt={ product.images.alt } 
+                    />
+                </div>
+                <div>
+                    <h1>{product.name}</h1>
+                    <p className="text-sm font-medium text-gray-900">
+                        <span className="sr-only">Cena:</span>
+                        {formatMoney(product.price / 100)}
+                    </p>
+                    <p className="text-sm text-gray-500">
+                        <span className="sr-only">Kategoria:</span> {product.description}
+                    </p>
+
+                    <div>
+                        <CheckIcon 
+                            className="h-5 w-5 flex-shrink-0 text-pink-500"
+                            aria-hidden="true"
+                        />
+                        <p className="ml-1 text-sm font-semibold text-slate-500">
+                            In stock
+                        </p>
+                    </div>
+
+                    <form action={AddToCartAction}>
+                        <input type="hidden" name="productId" value={product.id}/>
+                        <button className="mt-4 hover:shadow-md transition-shadow py-2 px-6 border rounded-sm shadow-sm bg-slate-300">Add to cart</button>
+                    </form>
+                </div>
+            </div>
+
             {recommended_products_filtered.length > 0 && (
-                <Suspense>
-                    <h2>Polecane produkty z kolekcji {collection_data.collection.name}</h2>
-                    <ProductList products={recommended_products_filtered.slice(0, 4)} containerName={containerName}/>
-                </Suspense>
+            <Suspense>
+                <h2>Polecane produkty z kolekcji {collection_data.collection.name}</h2>
+                <ProductList products={recommended_products_filtered.slice(0, 4)} containerName={containerName}/>
+            </Suspense>
             )}
-        </>
+        </article>
     );
 }
--- src/app/ui/organism/NavBar.tsx
+++ src/app/ui/organism/NavBar.tsx
@@ -1,8 +1,9 @@
 'use client'
-
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { ShoppingCart } from 'lucide-react';
 import { ActiveLink } from "../atoms/ActiveLink";
+
 
 type NavLink = {
     href: string;
@@ -19,63 +20,60 @@ const NavLinks: NavLink[] = [
     { href: "/categories/accessories/", label: "Accessories", exact: false },
 ]
 
+
 export function NavBar(): JSX.Element {
     const [searchQuery, setSearchQuery] = useState<string>("");
     const [typingTimer, setTypingTimer] = useState<NodeJS.Timeout | null>(null);
-    const delayTime = 500;
-
+    
+    const router = useRouter();
+    const delayTime = 500; 
+    
     const handleSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchQuery(event.target.value);
-    };
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const form = event.currentTarget;
-        form.submit();
+        setSearchQuery(event.target.value); 
     };
 
     const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
         const { value } = event.currentTarget;
+        setSearchQuery(value); 
         if (value.length >= 2) {
             if (typingTimer) {
                 clearTimeout(typingTimer);
             }
             const timer = setTimeout(() => {
-                const form = document.getElementById('searchForm') as HTMLFormElement;
-                if (form) {
-                    form.submit();
-                }
+                const queryParamString = new URLSearchParams({ query: value }).toString();
+                router.push(`/search?${queryParamString}`);
+                setSearchQuery("");
             }, delayTime);
             setTypingTimer(timer);
         }
     };
 
     return (
-    <nav className="bg-white h-60">
-        <div className="flex justify-between items-center mx-auto max-w-md px-12 lg:max-w-7xl h-full">
-            <ul className="flex space-x-4 h-full">
-                {NavLinks.map((link, index) => (
-                    <li key={index} className="w-32 text-center	border-b-2 border-transparent hover:border-red-500 flex items-center">
-                        <ActiveLink href={link.href} exact={link.exact}>{link.label}</ActiveLink>
-                    </li>
-                ))}
-            </ul>
-            <ul className="flex items-center">
-                <form id="searchForm" role='searchbox' method="GET" action="/search" onSubmit={handleSubmit} className="flex bg-white border border-gray-300 rounded-full">
-                    <input 
-                        type="text" 
-                        name="query"
-                        minLength={1}
-                        placeholder="Search"
-                        value={searchQuery}
-                        onChange={handleSearchInput}
-                        onKeyUp={handleKeyUp}
-                        className="bg-white text-gray-700 px-3 py-1 rounded-full focus:outline-none"
-                    />
-                </form>
-                <ShoppingCart className='ml-4 h-6 w-6 flex-shrink' aria-hidden="true"/>
-            </ul>
-        </div>
-    </nav>
+        <nav className="bg-white h-60">
+            <div className="flex justify-between items-center mx-auto max-w-md px-12 lg:max-w-7xl h-full">
+                <ul className="flex space-x-4 h-full">
+                    {NavLinks.map((link, index) => (
+                        <li key={index} className="w-32 text-center border-b-2 border-transparent hover:border-red-500 flex items-center">
+                            <ActiveLink href={link.href} exact={link.exact}>{link.label}</ActiveLink>
+                        </li>
+                    ))}
+                </ul>
+                <ul className="flex items-center">
+                    <div className="flex bg-white border border-gray-300 rounded-full">
+                        <input 
+                            type="text" 
+                            name="query"
+                            minLength={1}
+                            placeholder="Search"
+                            value={searchQuery}
+                            onChange={handleSearchInput}
+                            onKeyUp={handleKeyUp}
+                            className="bg-white text-gray-700 px-3 py-1 rounded-full focus:outline-none"
+                        />
+                    </div>
+                    <ShoppingCart className='ml-4 h-6 w-6 flex-shrink' aria-hidden="true"/>
+                </ul>
+            </div>
+        </nav>
     );
 }
