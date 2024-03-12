2eeb988368bd63c89c273a01862a8ff0f5016b7d
mpielak <michal.pielak81@gmail.com>
2024-03-12 09:52:41 +0100

    Poprawa styli

--- src/app/ui/atoms/SelectSortProducts.tsx
+++ src/app/ui/atoms/SelectSortProducts.tsx
@@ -22,26 +22,22 @@ export const SetSortDirection = () => {
     };
 
     return (
-        <div className="flex items-center mb-5">
-            <label htmlFor="sort-by-price" className="mr-3">
-                Sort by:
-            </label>
-            <select 
-                id="sort-by-price"
-                className="mr-3"
-                defaultValue={`price, ${order}`}
-                onChange={handleSetOrdering}
-            >   
-                <option value="default, asc">--Sort by--</option>
-                <option data-testid="sort-by-price" value="price, asc">Price asc</option>
-                <option data-testid="sort-by-price" value="price, desc">Price desc</option>
-                <option data-testid="sort-by-rating" value="rating, asc">Rating asc</option>
-                <option data-testid="sort-by-rating" value="rating, desc">Rating desc</option>
-            </select>
-
-            {order && orderby && (
-                <span className="ml-4 w-32">Choice orderby:&nbsp;{orderby}, sort direction:&nbsp;{order}</span>
-            )}
+        <div className="flex items-center">
+                <label htmlFor="sort-by-price" className="mr-3">
+                    Sort by:
+                </label>
+                <select 
+                    id="sort-by-price"
+                    className="mr-3"
+                    defaultValue={`price, ${order}`}
+                    onChange={handleSetOrdering}
+                >   
+                    <option value="default, asc">--Sort by--</option>
+                    <option data-testid="sort-by-price" value="price, asc">Price asc</option>
+                    <option data-testid="sort-by-price" value="price, desc">Price desc</option>
+                    <option data-testid="sort-by-rating" value="rating, asc">Rating asc</option>
+                    <option data-testid="sort-by-rating" value="rating, desc">Rating desc</option>
+                </select>
         </div>
     );
 };
